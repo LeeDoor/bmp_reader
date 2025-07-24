fff72f8eab1751b4ba5bd63878e3674b65eeabbf
leedoor <samoshenkoleonid@yandex.ru>
2025-07-24 19:46:45 +0300

    final

--- bmp_writer.cpp
+++ bmp_writer.cpp
@@ -8,10 +8,13 @@ BMPWriter::BMPWriter(const std::string& filename) {
 void BMPWriter::write_to_file(const BMP& bmp) {
     BMPMetadata metadata = bmp.metadata();
     const auto& matrix = bmp.value();
-    const size_t alignment = 4 - (bmp.width() * 3 % 4);
+    size_t alignment = 4 - (bmp.width() * 3 % 4);
+    alignment = alignment == 4 ? 0 : alignment;
     metadata.file_header.offset = sizeof(metadata);
+    metadata.info_header.bit_count = 24;
     ofs_.write(reinterpret_cast<const char*>(&metadata), sizeof(BMPMetadata));
-    for(const auto& row : matrix) {
+    for(auto it = matrix.rbegin(); it != matrix.rend(); ++it) {
+        const auto& row = *it;
         ofs_.write(reinterpret_cast<const char*>(row.data()), sizeof(Color) * bmp.width());
         ofs_.write("\0\0\0", alignment);
     }
